5c69492fe5a7897b7e2815370520d264c8a0df89
Ricardo Yamaguchi <ryamaguchi@ciandt.com>
2020-08-18 18:02:36 -0300

    Initial Commit

---
+++ PocSharingButton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PocSharingButton
+//
+//  Created by Ricardo Yamaguchi on 18/08/20.
+//  Copyright © 2020 Ricardo Yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
